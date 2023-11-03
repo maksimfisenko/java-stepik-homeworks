0d77dce331503c04e5705e15243c9a3a44352a7d
Maxim Fisenko <fisish@yandex.ru>
2023-11-03 04:29:35 +0300

    Added Homework 9

---
+++ src/homework9/TestClass.java
@@ -0,0 +1,16 @@
+package homework9;
+
+public class TestClass {
+    public static void main(String[] args) {
+        createTwoStrings();
+        createTwoStrings();
+        String s1 = "1";
+        String s2 = "2";
+        createTwoStrings();
+    }
+
+    public static void createTwoStrings() {
+        String s1 = "1";
+        String s2 = "2";
+    }
+}
